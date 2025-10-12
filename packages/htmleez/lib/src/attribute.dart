e1a428ed12ca0a4a676253a66e6cbe793fc42b60
Gustavo Guzman <guzmanlo.2001@gmail.com>
2025-10-12 18:17:55 +0200

    fix: coderabbit errors ...

--- packages/htmleez/lib/src/attribute.dart
+++ packages/htmleez/lib/src/attribute.dart
@@ -20,7 +20,6 @@ final class RawAttribute {
   HtmlComponent call([String? content]) {
     return RawAttributeComponent(name, content);
   }
-  }
 }
 
 final class EscapedAttributeComponent extends AttributeComponent {
